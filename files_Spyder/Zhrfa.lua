0f00b5d7f2738fda57896293eba1ebd23add6c4c
SpyderTele <76131660+SpyderTele@users.noreply.github.com>
2020-12-23 22:18:45 +0300

    Create Zhrfa.lua

---
+++ files_Spyder/Zhrfa.lua
@@ -0,0 +1,28 @@
+local function Zhrfa(msg)
+local text = msg.content_.text_
+if not database:get(bot_id..'Spyder:Zhrfa:Mute'..msg.chat_id_) then
+if text == "تعطيل الزخرفه" and Owner(msg) then
+send(msg.chat_id_, msg.id_, '⌔︙ تم تعطيل الزخرفه')
+database:set(bot_id.." MERO:zhrf_Bots"..msg.chat_id_,"close")
+end
+if text == "تفعيل الزخرفه" and Owner(msg) then
+send(msg.chat_id_, msg.id_,'⌔︙ تم تفعيل الزخرفه')
+database:set(bot_id.." MERO:zhrf_Bots"..msg.chat_id_,"open")
+end
+if text and text:match("^زخرفه (.*)$") and database:get(bot_id.." MERO:zhrf_Bots"..msg.chat_id_) == "open" then
+local TextZhrfa = text:match("^زخرفه (.*)$")
+zh = https.request('https://ahmedcg4.ml/Spyder/en.php?en='..URL.escape(TextZhrfa)..'')
+zx = JSON.decode(zh)
+t = "\n⌔︙قائمه الزخرفه \nٴ⌔︙○━━━Spyder━━━⌔︙○ٴ \n"
+i = 0
+for k,v in pairs(zx.ok) do
+i = i + 1
+t = t..i.."-  `"..v.."` \n"
+end
+send(msg.chat_id_, msg.id_, t..'ٴ○━━━l━━━○ٴ\n⌔︙اضغط على الاسم ليتم نسخه')
+end
+
+end
+return {
+Spyder = Zhrfa
+}
