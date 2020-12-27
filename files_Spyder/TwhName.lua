76e23ace04a3c92df77717b80f8a6c6ba5d632a2
SpyderTele <76131660+SpyderTele@users.noreply.github.com>
2020-12-27 18:06:28 +0300

    Update TwhName.lua

--- files_Spyder/TwhName.lua
+++ files_Spyder/TwhName.lua
@@ -9,26 +9,26 @@ user_id_ = user_id
 end
 if text and text:match("^وضع توحيد (.*)$") and Owner(msg) and database:get(bot_id.."kt:twh:stats"..msg.chat_id_) == "open" then
 local teh = text:match("^وضع توحيد (.*)$")
-send(msg.chat_id_, msg.id_,'⌔︙تم تعيين '..teh..' كتوحيد للمجموعه')
+send(msg.chat_id_, msg.id_,'☑️┇تم تعيين'..teh..' كتوحيد للمجموعه')
 database:set(bot_id.."Spyder:teh"..msg.chat_id_,teh)
 end
 if text and text:match("^تعين عدد الكتم (.*)$") and Owner(msg) and database:get(bot_id.."kt:twh:stats"..msg.chat_id_) == "open" then
 local nump = text:match("^تعين عدد الكتم (.*)$")
-send(msg.chat_id_, msg.id_,'⌔︙تم تعين  '..nump..' عدد الكتم')
+send(msg.chat_id_, msg.id_,'☑️┇تم تعين  '..nump..' عدد الكتم')
 database:set(bot_id.."Spyder:nump"..msg.chat_id_,nump)
 end
 if text == "التوحيد" then
 local s1 = database:get(bot_id.."Spyder:teh"..msg.chat_id_) or "لا يوجد توحيد"
 local s2 = database:get(bot_id.."Spyder:nump"..msg.chat_id_) or 5
-send(msg.chat_id_, msg.id_,'⌔︙التوحيد هو '..s1..'\n ⌔︙ عدد الكتم هو : '..s2)
+send(msg.chat_id_, msg.id_,'🔘┇التوحيد هو '..s1..'\n 💢┇عدد الكتم هو : '..s2)
 end
 end
 if text == "تفعيل التوحيد" and Constructor(msg) then
-send(msg.chat_id_, msg.id_, '⌔︙تم تفعيل التوحيد')
+send(msg.chat_id_, msg.id_, '☑️┇تم تفعيل التوحيد')
 database:set(bot_id.."kt:twh:stats"..msg.chat_id_,"open")
 end
 if text == "تعطيل التوحيد" and Constructor(msg) then
-send(msg.chat_id_, msg.id_, '⌔︙تم تعطيل التوحيد')
+send(msg.chat_id_, msg.id_, '☑️┇تم تعطيل التوحيد')
 database:set(bot_id.."kt:twh:stats"..msg.chat_id_,"close")
 end
 if not Constructor(msg) then
@@ -45,7 +45,7 @@ if (tonumber(tshake_nnn2) == tonumber(tshake_nnn) or tonumber(tshake_nnn2) > ton
 database:sadd(bot_id.."Spyder:Muted:User"..msg.chat_id_, msg.sender_user_id_)
 else 
 database:incrby(bot_id.."Spyder:nump22"..msg.chat_id_..msg.sender_user_id_,1)
-send(msg.chat_id_, msg.id_, "⌔︙عزيزي >>["..tshake2.username_.."](https://t.me/"..(tshake2.username_ or "tshaketeam")..")\n⌔︙عليك وضع التوحيد ~⪼ {`"..database:get(bot_id.."Spyder:teh"..msg.chat_id_).."`} بجانب اسمك\n⌔︙عدد المحاولات المتبقيه {"..(tonumber(tshake_nnn) - tonumber(tshake_nnn2)).."}")
+send(msg.chat_id_, msg.id_, "👤┇عزيزي >>["..tshake2.username_.."](https://t.me/"..(tshake2.username_ or "tshaketeam")..")\n🔘┇عليك وضع التوحيد ~⪼ {`"..database:get(bot_id.."Spyder:teh"..msg.chat_id_).."`} بجانب اسمك\n📌┇عدد المحاولات المتبقيه {"..(tonumber(tshake_nnn) - tonumber(tshake_nnn2)).."}")
 end
 end
 end
